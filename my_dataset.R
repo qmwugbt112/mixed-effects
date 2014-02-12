99c618c7452bcda39656140d58c43d41c0975c8d
qmwugbt112 <richardnichols1@Geneflow.local>
2014-02-12 13:16:10 +0000

    Year effect added
    Corrected error which led to no year effect

--- my_dataset.R
+++ my_dataset.R
@@ -6,7 +6,7 @@
 Ntrees<-6
 Nyears<-10
 Tmeans<-rnorm(Ntrees,100,20)				# tree performance mean 100, sd 20
-Ymeans<-rnorm(Nyears,0,2)	  				# difference over years mean 0, sd 10
+Ymeans<-rnorm(Nyears,0,2)	  				# difference over years mean 0, sd 2
 Ssize<-4									# sample size of apples from each tree
 FeffectM<-.5								# weight increase per unif of fertiliser each year
 FeffectC<-10								# intercept of fertilizer effect
@@ -20,7 +20,7 @@ Fdose<-vector(length=length(YearID))
 appleWT<-vector(length=Nyears*Ntrees*Ssize)			# vector to store apple weights
 
 #Add in the different random * fixed effects
-for (i in 1:Nyears) appleWT[YearID==i]<-Ymeans[i]
+for (i in 1:Nyears) appleWT[YearID==levels(YearID)[i]]<-Ymeans[i]
 for (i in 1:Ntrees) {
 		appleWT[TreeID==i]<-appleWT[TreeID==i]+Tmeans[i]
 		}
