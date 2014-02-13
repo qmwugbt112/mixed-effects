eac897f3e9cc6a61907e53b0ee7396763e016679
Richard Nichols <r.a.nichols@qmul.ac.uk>
2014-02-13 14:59:51 +0000

    Larger year effect
    Improved datasets by increasing the size of the random effect in each
    year - giving more obvious difference from the fixed effects model.
    Also tidied up code for the loops by giving the vectors of random
    effects the names of the corresponding factor levels

--- my_dataset.R
+++ my_dataset.R
@@ -6,22 +6,24 @@
 Ntrees<-6
 Nyears<-10
 Tmeans<-rnorm(Ntrees,100,20)				# tree performance mean 100, sd 20
-Ymeans<-rnorm(Nyears,0,2)	  				# difference over years mean 0, sd 2
+Ymeans<-rnorm(Nyears,0,20)	  				# difference over years mean 0, sd 20
 Ssize<-4									# sample size of apples from each tree
 FeffectM<-.5								# weight increase per unif of fertiliser each year
 FeffectC<-10								# intercept of fertilizer effect
 
-TreeID<-rep(1:Ntrees,Nyears,each=Ssize)  	# unique number for each tree
-TreeID<-factor(TreeID)						
+TreeID<-factor(
+		rep(1:Ntrees,Nyears,each=Ssize))  	# unique number for each tree
+
 YearID<-factor(rep(2000+(1:Nyears),each=Ntrees*Ssize)) 	# year
 Fdose<-vector(length=length(YearID))
 	for (i in levels(YearID)) Fdose[YearID==i]<-rnorm(1,30,5)			# dose of fertiliser on each year
 	
 appleWT<-vector(length=Nyears*Ntrees*Ssize)			# vector to store apple weights
-
+names(Ymeans)<-levels(YearID)						# associate each mean with the year name
+names(Tmeans)<-levels(TreeID)						# associate each mean with a tree name
 #Add in the different random * fixed effects
-for (i in 1:Nyears) appleWT[YearID==levels(YearID)[i]]<-Ymeans[i]
-for (i in 1:Ntrees) {
+for (i in levels(YearID)) appleWT[YearID==i]<-Ymeans[i]
+for (i in levels(TreeID)) {
 		appleWT[TreeID==i]<-appleWT[TreeID==i]+Tmeans[i]
 		}
 
