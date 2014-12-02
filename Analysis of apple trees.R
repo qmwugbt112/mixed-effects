ce022b4c2105373fe25dec1cd62fbd372c1d5be3
qmwugbt112 <richardnichols1@Geneflow.local>
2014-12-02 23:07:22 +0000

    Research Methods 2015
    Introduces a more gentle intro to lm

--- Analysis of apple trees.R
+++ Analysis of apple trees.R
@@ -1,87 +1,96 @@
-# Before running this code, generate your own personal dataset using 
-# the R script my_dataset
-# It is a script to generate data in which an orchard has 
-# a sample of apples weighed in each year from each tree.
-# All trees are fertilized each year, but the amount of
-# fertilizer per tree varies slightly among years
 
-# Try this
-source(file.choose())
+# For the initial exercise generate your own unique dataset for tree1, 2 & 3
 
-# or set your working directory to the appropriate folder and use
-# source('my_dataset.R')
+apples1<-rnorm(40,rnorm(1,20,3),rnorm(1,5))
+apples2<-rnorm(40,rnorm(1,40,3),rnorm(1,10))
+apples3<-rnorm(40,rnorm(1,50,3),rnorm(1,15))
 
+par(mfrow=c(3,1))
+hist(apples1,xlim=c(0,100),breaks=seq(0,150,5), border='green')
+hist(apples2,xlim=c(0,100),breaks=seq(0,150,5), border='red')
+hist(apples3,xlim=c(0,100),breaks=seq(0,150,5), border='blue')
 
+# use the mean() and sd() commands to obtain the mean and SD of each distribution
+# compare them with your neighbour
 
-attach(MyData)
+# use lm() command to find the mean & residual variation for each of apples1-3
+# e.g. mod1<-lm(apples1~1); summary(mod1)
+# Make sure you understand what the residual standard error is
+# make sure you understand what the intercept and 'Std. Error' (of the intercept) is, and why it is different
 
+# How do these values relate to the mean and sd you calculated?
 
-# Get an idea of the data by printing it out
-head(MyData,30)
+# Now bind your dataset into one long vector, and create a factor to distinguish the data from each tree
+allApples<-c(apples1,apples2,apples3)
+treef<-factor(rep(c('Tree1','Tree2','Tree3'),each=40))
+# print them out to have a look by typing their names 'allApples' and 'treef'
 
+# use mod4<-lm(allApples~treef) to fit all 3 tree means.
+# use the summary() command to look at the results
+# Which mean is the intercept equal to, why?
+# Why are the values relating to the other two means not the same as you found before?
+# Why are the standard errors for these two values the same, when they have different variances?
 
-
-# use install.packages(c('nlme','lattice'),dependencies=T) if necessary
-# Then fire up the packages nlme
-
+# Install the library allowing you to run mixed effects models
 library(nlme)
-library(lattice)
+
+# Fit a simple nlme
+mod5<-lme(allApples~1,random=~1|treef)
+
+# Examine the result using the summary() command
+# what is the intercept of the fixed effect equal to ?
+# what do the StdDev:    (Intercept) Residual refer to
+# Use the fitted() command to see what the predictions are for each tree
+# Why are these not equal to the means of each tree?
 
 
+# run this command to modify your data so that weight of each apple depends 
+# on its height in the tree (don't worry too much if you don't understand it)
 
-M1<-lm(appleWT~YearID)
-M2<-lme(appleWT~1,random=~1|YearID)
+allApplesh<-rep(0,120)
+height<-runif(120,1,3)
+for (i in c('Tree1','Tree2','Tree3')) {
+				residV<-allApples[treef==i]-mean(allApples[treef==i])
+				treeM<-mean(allApples[treef==i])
+				heightEffect<-height[treef==i]*runif(1,20,40)+runif(1,0,5)
+				allApplesh[treef==i]<-abs(treeM+heightEffect)/35+residV
+				}
 
-# Have a look at the overall relationship with fertilizer does
-xyplot(appleWT+fitted(M1)+fitted(M2)~Fdose)
+ # enable lattice plots
+ library(lattice)
 
-# Split it down by tree
-xyplot(appleWT+fitted(M1)+fitted(M2)~Fdose|TreeID)
+# Use xyplot to examine the data
+xyplot(allApplesh~height|treef)
 
+# Fit a different fixed effect of height for each tree, plus the random effects
+mod6<-lme(allApplesh~height+height/treef,random=~1|treef)
+mod7<-lm(allApplesh~height*treef)
+xyplot(allApplesh+fitted(mod6)+fitted(mod7)~height|treef)
 
+# Advanced topic: Why do the fitted lines from lm and lme differ from each other
+# generate a new modified dataset if your effect is not obvious
 
-# print out the two models
-summary(M1)
-summary(M2)
+##################################################################
+# Applying this logic to genetic data
 
-# use anova(M1) to find the mean square of the fixed effects model
-# square root it to find the pooled estimate of the SD.
-# how does this compare with the SD in M2
-# what is the other SD in M2?
-# what is the intercept?
+# Generate some genotypes
+nIndivs<-20;nLoci=40
+genotypes<-matrix(sample(-1:1,nIndivs*nLoci,T),nrow=nIndivs)
 
-# are the estimated means for each year in M1 and M2 identical
-# use coef(M1) & coef(M2) to see
+# look at the genotype data using the head(command)
+locusEffects<-rnorm(nLoci,sd=3)
 
-# construct a fixed effects (lm) and random effects (lme)
-# model to model the effect of tree instead of year
-# call these Models M3 and M4
+# Use matrix multiplication to obtain expected phenotype
+expectedWT<-100+genotypes %*% locusEffects
 
+# examine expectedWT
 
-# use this command to plot them
+# add the environmental variation to get the phenotype
+phenotypeWT<-expectedWT+rnorm(nIndivs,sd=2)
 
-xyplot(appleWT+fitted(M3)+fitted(M4)~TreeID)
-
-# Why is the shrinkage different in different trees?
-
-# Try fitting TreeID, YearID and Fdose like this
-M7a<-lm(appleWT~TreeID+YearID+Fdose+VarID)
-
-#  Look at M7a. What has gone wrong?
-# Why can't the fixed effect model do
-# all that you asked
+mod10<-lme(phenotypeWT~1,random=~genotypes|1)
 
 
-# Fit Fdose alone in a fixed effects model
-# but both Fdose and YearID as a random effect!
-M7<-lm(appleWT~Fdose+TreeID)
-M8<-lme(appleWT~Fdose+TreeID,random=~1|YearID)
-
-xyplot(appleWT+fitted(M7)+fitted(M8)~Fdose|TreeID)
-
-# Why is it possible to fit both Fdose and YearID 
-# in a random effects model ??
-# Why might that be apropriate ?
 
 ######################################################
 ########### Exercise ends ############################
--- genomic estimation.R
+++ genomic estimation.R
@@ -1,5 +1,9 @@
 # if necessary install rrBLUP
-install.packages(rrBLUP,dependencies=T)
+<<<<<<< HEAD
+# install.packages('rrBLUP',dependencies=T)
+=======
+install.packages('rrBLUP',dependencies=T)
+>>>>>>> 2e6386641e9327231b3d0acf04080ee36c9a3678
 
 # load the library
 library(rrBLUP)
@@ -14,30 +18,35 @@ Hdat[1:10,1:10]
 Gmat<-as.matrix(Hdat[,5:ncol(Hdat)])
 dim(Gmat)
 
-########## Richard's quick and dirty imputation###################
+Gmat[1:10,1:10]
 
-# Calculate the mean of each col, ignoring NAs
-colmns<-colMeans(Gmat,na.rm=T)
-# Find out where the NAs are 
-naLocn<-which(is.na(Gmat))
-# Convert each location to a col number
-colNos<-(naLocn+nrow(Gmat)-1)%/%nrow(Gmat)
-# Fill in missing values with corresponding mean
-Gmat[naLocn]<-colmns[colNos]
+########## Richard's quick and dirty imputation###################
+######### Replace missing values with the column mean #########
+Gmat[is.na(Gmat)]<-rep(colMeans(Gmat,na.rm=T),colSums(is.na(Gmat)))
+
 ##################################################################
+
+
 
 #Check the NAs have been replaced by the column mean
 Gmat[1:10,1:10]
 
-
+# Get the data on the penotypic trait to be analysed (Weight in this case)
 Weight<-Hdat$Weight
-# Find row numbers for lines with missing values
-missing.weight<-which(is.na(Weight))
 
-#define the training and test populations
+############ This chunk of code decides which individuals #######
+############ to leave out of the training dataset         #######
+																#
+# Find row numbers for lines with missing values 					
+# They can't be used for training 			
+missing.weight<-which(is.na(Weight))							
+																
+#define the training and test populations						
 #training-60% validation-40%
-nLines<-length(Weight)
+
+
 #make a random selection of 60% of the grasshoppers
+nLines<-length(Weight)
 train= sample(1:nLines,(.6)*(nLines))
 
 # remove lines with missing values from training set
@@ -45,36 +54,40 @@ train<-setdiff(train,missing.weight)
 
 # get the numbers of the test lines (those not in train)
 test<-setdiff(1:nLines,train)
+																	#
+#####################################################################
+#####################################################################
 
 # extract subset of lines and genotypes for training
 trainWeight<-Weight[train]
 trainGenot<-Gmat[train,]
 
-# Do rrBLUP, see http://www.inside-r.org/packages/cran/rrBLUP/docs/mixed.solve
+# Do rrBLUP, see 
+# http://www.inside-r.org/packages/cran/rrBLUP/docs/mixed.solve
 library(rrBLUP)
 GCA_BLUP<-mixed.solve(trainWeight,Z=trainGenot,K = NULL, SE = FALSE, return.Hinv=FALSE)
 
 # Show that estimated effects matrix-multiplied by genotype is the prediction
 
-
-
 est1<-as.vector(trainGenot%*%GCA_BLUP$u)
-
-
 plot(est1,trainWeight)
 
 
+# Find out how good the correlation is
 mod1<-lm(trainWeight~0+est1)
 abline(mod1)
 anova(mod1)
 
+
+# Now repeat the process for the test dataset 
+# (individuals not used to train the algorithm)
 testWeight<-Weight[test]
 testGenot<-Gmat[test,]
 est2<-as.vector(testGenot%*%GCA_BLUP$u)
 
 plot(est2,testWeight)
 
-
+# How good is this correlation?
 mod2<-lm(testWeight~0+est2)
 abline(mod2)
 anova(mod2)
